e82d4331258d46848d5de2e8a5cc99a0820b5d76
nurkholis <mr.kholis@outlook.com>
2021-02-03 00:32:00 +0700

    menu button fixed

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ $(document).ready(function(){
         // sticky navbar on scroll script
         if(window.scrollY > 20){
             $('.navbar').addClass("sticky");
-            $('.buttonmenu').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+        }
+
+        if(window.scrollY > 20){
+            $('.buttonmenu').addClass("sticky");
+        }else{
             $('.buttonmenu').removeClass("sticky");
         }
-    })
+
+    });
+
+    //toggle menu navbar
+        $('.buttonmenu').click(function(){
+            $('.navbar .menu').toggleClass("active");
+            $('.buttonmenu i').toggleClass("active");
+        });
+
 });
