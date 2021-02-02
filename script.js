96cab216c73506a86fda39aa8c119852787ea8b3
nurkholis <mr.kholis@outlook.com>
2021-02-02 14:01:27 +0700

    add button

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
         // sticky navbar on scroll script
         if(window.scrollY > 20){
             $('.navbar').addClass("sticky");
+            $('.buttonmenu').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+            $('.buttonmenu').removeClass("sticky");
         }
     })
 });
